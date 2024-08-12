2c1f66409a38098b34fb90e4a0dee2b796cee30d
Yogev Sawa <4950n2012e@online.de>
2024-08-12 10:11:50 +0200

    made project file update a function (amend_project_update)

--- share/amend/project/__init.lua
+++ share/amend/project/__init.lua
@@ -37,66 +37,69 @@ for group, t in pairs(CONFIG.EXTENSIONS) do
 end
 
 -- [[ UPDATE/SAVE SETTINGS ]]
-if PROJECT.UPDATE then
-    message(TRACE[1], 'updating project file...')
+function amend_project_update() 
+    if PROJECT.UPDATE then
+        message(TRACE[1], 'updating project file...')
 
-    -- cleanup
-    PROJECT.UPDATE = nil
-    table.unique(PROJECT.USES)
-    table.sort(PROJECT.USES)
+        -- cleanup
+        PROJECT.UPDATE = nil
+        table.unique(PROJECT.USES)
+        table.sort(PROJECT.USES)
 
-    local usage = {
-        PROJECT = [=[
-    Project settings.
+        local usage = {
+            PROJECT = [=[
+        Project settings.
 
-    This table contains two required entries:
+        This table contains two required entries:
 
-        NAME            Project name.
-        VERSION         Project version ({major, minor[, patch[, tweak]]}).
+            NAME            Project name.
+            VERSION         Project version ({major, minor[, patch[, tweak]]}).
 
-    as well as 
+        as well as 
 
-        USES            List of tools in use.
+            USES            List of tools in use.
 
-    Users are free to add additional entries.
-]=],
-        CONFIG = [=[
-    Amend configuration.
+        Users are free to add additional entries.
+    ]=],
+            CONFIG = [=[
+        Amend configuration.
 
-]=],
-        TOOLS = [=[
-    Tools.
-]=],
-        PATHS = [=[
-    Package paths.
+    ]=],
+            TOOLS = [=[
+        Tools.
+    ]=],
+            PATHS = [=[
+        Package paths.
 
-    Here, additional Lua paths may be listed, if necessary.
-]=]
-    }
+        Here, additional Lua paths may be listed, if necessary.
+    ]=]
+        }
 
-    local function comment(f, sec, paragraph)
-        if paragraph then
-            f:write("\n")
+        local function comment(f, sec, paragraph)
+            if paragraph then
+                f:write("\n")
+            end
+
+            if usage[sec] then
+                f:write("--[=[\n")
+                f:write(usage[sec])
+                f:write("]=]\n")
+            end
         end
 
-        if usage[sec] then
-            f:write("--[=[\n")
-            f:write(usage[sec])
-            f:write("]=]\n")
-        end
-    end
-
-    local f = io.open(PROJECTFILE, "w")
-    for i, sec in ipairs {"PROJECT", "CONFIG", "TOOLS", "PATHS"} do
-        comment(f, sec, i > 1)
-        if sec == "TOOLS" then
-            f:write("TOOLS = ")
-            io.dump(_G[sec] or {}, {stream = f, quoted = true})
-        else
-            io.dump(_G[sec] or {}, {key = sec, stream = f})
+        local f = io.open(PROJECTFILE, "w")
+        for i, sec in ipairs {"PROJECT", "CONFIG", "TOOLS", "PATHS"} do
+            comment(f, sec, i > 1)
+            if sec == "TOOLS" then
+                f:write("TOOLS = ")
+                io.dump(_G[sec] or {}, {stream = f, quoted = true})
+            else
+                io.dump(_G[sec] or {}, {key = sec, stream = f})
+            end
         end
     end
 end
+amend_project_update() 
 
 -- [[ LOAD TOOLS ]]
 for _, tag in ipairs(PROJECT.USES) do
